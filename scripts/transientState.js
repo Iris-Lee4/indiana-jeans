492afb1d542c418b9a5f0cfc044fcd000af33607
Iris Lee <irislee4git@gmail.com>
2024-05-10 09:42:49 -0400

    update transient state to use key value pairs instead of object

--- scripts/transientState.js
+++ scripts/transientState.js
@@ -1,28 +1,39 @@
 // Set up the transient state data structure and provide initial values
-const transientState = {
-    "ownsBlueJeans": false,
-    "socioLocationId": 0, 
-}
+// const transientState = {
+//     "ownsBlueJeans": false,
+//     "socioLocationId": 0, 
+// }
+
+//use map to store transient state with key value pairs
+
+const transientState = new Map();
+transientState.set("ownsBlueJeans", false);
+transientState.set("socioLocationId", 0)
 
 // Functions to modify each property of transient state
 export const setOwnsBlueJeans = (chosenOwnership) => {
-    transientState.ownsBlueJeans = chosenOwnership
+    // transientState.ownsBlueJeans = chosenOwnership
+    transientState.set("ownsBlueJeans", chosenOwnership)
     console.log(transientState)
 }
 
 export const setSocioLocationId = (chosenLocation) => {
-    transientState.socioLocationId = chosenLocation
+    // transientState.socioLocationId = chosenLocation
+    transientState.set("socioLocationId", chosenLocation)
     console.log(transientState)
 }
 
 // Function to convert transient state to permanent state
 export const saveSurveySubmission = async () => {
+
+    const transientStateObject = Object.fromEntries(transientState);
+
     const postOptions = {
         method: "POST",
         headers: {
             "Content-type": "application/json"
         },
-        body: JSON.stringify(transientState)
+        body: JSON.stringify(transientStateObject)
     }
 
     const response = await fetch("http://localhost:8088/submissions", postOptions)
